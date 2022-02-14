68c62089616bfd7ffc30e5bfe4b3253fd4710b6f
frank <frank@LAPTOP-M8EU7G13.localdomain>
2022-02-14 19:37:42 +0100

    dokument mit dropdow zu der dropbox

--- webanwendung/BWI520_Praktikum_Anwendungssysteme/src/de/hwg_lu/bw4s/beansPruefung/HtmlExpresion.java
+++ webanwendung/BWI520_Praktikum_Anwendungssysteme/src/de/hwg_lu/bw4s/beansPruefung/HtmlExpresion.java
@@ -23,15 +23,15 @@ public class HtmlExpresion {
 		html += "                    <li><a href=\"../jsp/willko.jsp\">HOME</a></li>";
 		html += "                    <li><a href=\"../jsp/aboutus.jsp\">ABOUT</a></li>";
 		html += "                    <div class=\"dropdown\">\r\n"
-				+ "    <button class=\"dropbtn\">Document \r\n"
-				+ "      <i class=\"fa fa-caret-down\"></i>\r\n"
-				+ "    </button>\r\n"
+				+ "    <li> \r\n"
+				+ "      <a href=\"https://www.dropbox.com/sh/vsm2rp4llvb8gyh/AABBwGD2Aas9nveKbctkW4tea?dl=0\">DOCUMENT</a>\r\n"
+				+ "    </li>\r\n"
 				+ "    <div class=\"dropdown-content\">\r\n"
-				+ "      <a href=\"https://www.dropbox.com/sh/05nrjrq5xmu38du/AAD3EBYU20zE7SDbdv85u1zIa?dl=0\">1.Semester</a>\r\n"
-				+ "      <a href=\"https://www.dropbox.com/sh/ydwr1vi5nhpvye6/AAD3w21Pq-eTOwhokP3TwERqa?dl=0\">2.Semester</a>\r\n"
-				+ "      <a href=\"https://www.dropbox.com/sh/7sigh0lc9kg72u9/AABSwDABkVqT2GZ0IH3JI1t5a?dl=0\">3.Semester</a>\r\n"
-				+ "      <a href=\"https://www.dropbox.com/sh/eqzov9iu0myllyk/AAAE3pXa9vMhqmUsKTa0b8Q2a?dl=0\">4.Semester</a>\r\n"
-				+ "      <a href=\"https://www.dropbox.com/sh/vui2paucod8n7u5/AADa9qqBDEnqEhvGQ53c9riEa?dl=0\">5.Semester</a>\r\n"
+				+ "      <a href=\"https://www.dropbox.com/sh/05nrjrq5xmu38du/AAD3EBYU20zE7SDbdv85u1zIa?dl=0\">1. Semester</a>\r\n"
+				+ "      <a href=\"https://www.dropbox.com/sh/ydwr1vi5nhpvye6/AAD3w21Pq-eTOwhokP3TwERqa?dl=0\">2. Semester</a>\r\n"
+				+ "      <a href=\"https://www.dropbox.com/sh/7sigh0lc9kg72u9/AABSwDABkVqT2GZ0IH3JI1t5a?dl=0\">3. Semester</a>\r\n"
+				+ "      <a href=\"https://www.dropbox.com/sh/eqzov9iu0myllyk/AAAE3pXa9vMhqmUsKTa0b8Q2a?dl=0\">4. Semester</a>\r\n"
+				+ "      <a href=\"https://www.dropbox.com/sh/vui2paucod8n7u5/AADa9qqBDEnqEhvGQ53c9riEa?dl=0\">5. Semester</a>\r\n"
 				+ "    </div>\r\n"
 				+ "  </div> ";
 		html += "                    <li><a href=\"../jsp/kontakt.jsp\">CONTACT</a></li>";
